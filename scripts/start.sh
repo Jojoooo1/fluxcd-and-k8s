627a1580295980ed443affb71cdff7339853f204
Jojoooo1 <chevalier.jonathan@live.fr>
2020-04-16 15:32:59 -0300

    added tag to imageé

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,3 +9,5 @@ kubectl apply -k ./staging/flux/ && kubectl -n flux-system rollout status deploy
 # Gets the public SSH Keys to authorize flux to commit to github
 kubectl -n flux-system logs deployment/flux | grep identity.pub | cut -d '"' -f2
 # fluxctl identity --k8s-fwd-ns flux
+# kubectl -n staging port-forward deployment/java-demo-backend-service 8080:8080
+# kubectl -n staging port-forward service/java-demo-database-service 3306:3306
