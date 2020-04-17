a2d7c152f3246cdf87004faf85c951906423968e
Jojoooo1 <chevalier.jonathan@live.fr>
2020-04-17 09:02:52 -0300

    wip

--- scripts/start.sh
+++ scripts/start.sh
@@ -4,7 +4,7 @@ set -e
 DIR=$PWD
 cd ..
 # Starts flux operator
-kubectl apply -k ./staging/flux/ && kubectl -n flux-system rollout status deployment/flux
+kubectl apply -k ./staging/minikube/flux/ && kubectl -n flux-system rollout status deployment/flux
 
 # Gets the public SSH Keys to authorize flux to commit to github
 kubectl -n flux-system logs deployment/flux | grep identity.pub | cut -d '"' -f2
