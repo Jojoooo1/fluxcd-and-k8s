eafc2afc36fa93a94b38dbfefe7f620f0f15f0b4
Jojoooo1 <chevalier.jonathan@live.fr>
2020-04-20 16:56:44 -0300

    wip

---
+++ environments/minikube/scripts/start-minikube.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+DIR=$PWD
+
+# Starts flux operator (did not used kubectl because it uses v2.0.3)
+kustomize build ../ | kubectl apply -f -
+# Verify deployment
+kubectl -n flux-system rollout status deployment/flux
+# Gets the public SSH Keys to authorize flux to commit to github
+kubectl -n flux-system logs deployment/flux | grep identity.pub | cut -d '"' -f2
+
+# Exposes the external IP directly to any program running on the host in order to set a LoadBalancer
+minikube tunnel
+
+# fluxctl identity --k8s-fwd-ns flux
+# kubectl -n staging port-forward deployment/java-demo-backend-service 8080:8080
+# kubectl -n staging port-forward service/java-demo-database-service 3306:3306
