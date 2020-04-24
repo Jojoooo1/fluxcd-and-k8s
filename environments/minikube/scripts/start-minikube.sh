957512bbcc0e47abe45f51ec4f0df21e9892880f
Jojoooo1 <chevalier.jonathan@live.fr>
2020-04-24 08:57:33 -0300

    wip

--- environments/minikube/scripts/start-minikube.sh
+++ environments/minikube/scripts/start-minikube.sh
@@ -18,10 +18,20 @@ fi
 
 # Starts flux operator (did not used kubectl because it uses Kustomize v2.0.3)
 kustomize build ../flux | kubectl apply -f -
-# Verify deployment status
+
+echo ">>> Waiting for flux to start"
 kubectl -n flux-system rollout status deployment/flux
-# Synchronize deployment
+echo ">>> flux is ready"
+
+echo ">>> Waiting for flux to sync"
 fluxctl sync --k8s-fwd-ns flux-system
+echo ">>> flux sync is done"
+
+echo ">>> Waiting for istiod to start"
+until kubectl -n istio-system get deploy istiod | grep "1/1"; do
+  sleep 5
+done
+echo ">>> Istio control plane is ready"
 
 # Gets the public SSH Keys to authorize flux to commit to github
 # kubectl -n flux-system logs deployment/flux | grep identity.pub | cut -d '"' -f2 # Replaced by a personal key in order to keep a single ssh key in github
