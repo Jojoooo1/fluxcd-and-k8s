06df37665672bcad0d20a67c6bc2261cce4954b9
Jojoooo1 <chevalier.jonathan@live.fr>
2020-04-24 08:04:09 -0300

    wip

--- environments/minikube/scripts/start-minikube.sh
+++ environments/minikube/scripts/start-minikube.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 set -e
 
-DIR=$PWD
+if [[ ! -x "$(command -v kubectl)" ]]; then
+  echo "kubectl not found"
+  exit 1
+fi
 
-# Creates Istio CRD first
-kubectl apply -f ../../../bases/istio/istio-manifest-crd.yaml
+if [[ ! -x "$(command -v kubkustomizeectl)" ]]; then
+  echo "kustomize not found"
+  exit 1
+fi
 
-# Starts flux operator (did not used kubectl because it uses v2.0.3)
-kustomize build ../ | kubectl apply -f -
+if [[ ! -x "$(command -v fluxctl)" ]]; then
+  echo "fluxctl not found"
+  exit 1
+fi
 
+# Starts flux operator (did not used kubectl because it uses Kustomize v2.0.3)
+kustomize build ../flux | kubectl apply -f -
 # Verify deployment
 kubectl -n flux-system rollout status deployment/flux
+# Synchronize deployment
+fluxctl sync --k8s-fwd-ns flux-system
 
 # Gets the public SSH Keys to authorize flux to commit to github
 # kubectl -n flux-system logs deployment/flux | grep identity.pub | cut -d '"' -f2 # Replaced by a personal key in order to keep a single ssh key in github
+# fluxctl identity --k8s-fwd-ns flux
 
 # Exposes the external IP directly to any program running on the host in order to set a LoadBalancer
 # minikube tunnel
 
-# fluxctl identity --k8s-fwd-ns flux
 # kubectl -n staging port-forward deployment/java-demo-backend-service 8080:8080
 # kubectl -n staging port-forward service/java-demo-database-service 3306:3306
